c5e9b7bb8bbdcb9e619a04319f0a196baa3ebfac
tom901 <thomas.paraiso@me.com>
2019-04-28 16:50:48 +0200

    Updates

---
+++ precache-manifest.4d6386af152e9a70c0e559f10de13312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f25ce3e6118942ab2b7",
+    "url": "/static/js/main.4f25ce3e.chunk.js"
+  },
+  {
+    "revision": "df7e46c0995545dfacda",
+    "url": "/static/js/1.df7e46c0.chunk.js"
+  },
+  {
+    "revision": "4f25ce3e6118942ab2b7",
+    "url": "/static/css/main.e8d9d67e.chunk.css"
+  },
+  {
+    "revision": "df7e46c0995545dfacda",
+    "url": "/static/css/1.b13e3d2e.chunk.css"
+  },
+  {
+    "revision": "dabb47e49f0321e751869533811228f7",
+    "url": "/index.html"
+  }
+];
